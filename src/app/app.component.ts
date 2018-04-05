6a4f0a1a193af351cb484e26dcf8e908654fda5b
AnnaFlourou <flourou.anna@gmail.com>
2018-04-05 19:43:32 +0200

    add bootstrap, linkAPI & create  choose-heo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  results = '';
+  // azerty = '';
+  constructor(/*private http: HttpClient*/) {
+  }
+  ngOnInit(): void {
+    /*this.http.get('https://akabab.github.io/superhero-api/api/id/729.json').subscribe(data => {
+      console.log(data);
+      this.azerty = data.images.md;
+  });*/
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { ChooseHeroComponent } from './choose-hero/choose-hero.component';
 
+import { RouterModule, Routes } from '@angular/router';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+const appRoutes: Routes = [
+  { path: 'choose-hero', component: ChooseHeroComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChooseHeroComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }

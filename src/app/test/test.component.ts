84678e20678580515987d8e9663e271373db5764
AnnaFlourou <flourou.anna@gmail.com>
2018-04-06 06:29:53 +0200

    add interactive game

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -11,6 +11,8 @@ export class TestComponent implements OnInit {
   hero; // hero par rapport a son id
   selectPlayer = null;
   selectIa = null;
+  healthPlayer = 500;
+  healthIa = 500;
   constructor(private apiService: ApiService) { }
   getPerso(param) {
     this.selectPlayer = this.heroes[param];
@@ -52,6 +54,55 @@ export class TestComponent implements OnInit {
         break;
     }
     console.log(this.selectIa);
+  }
+  usePower() {
+    console.log(this.selectPlayer.powerstats.power);
+    console.log(this.selectIa.powerstats.durability);
+    console.log(this.selectIa.powerstats.power);
+    const sub = this.selectPlayer.powerstats.power - this.selectIa.powerstats.durability;
+    console.log(sub);
+    if (sub <= 0) {
+      this.healthIa -= Math.floor(this.selectPlayer.powerstats.power / 2);
+    } else {
+      this.healthIa -= sub;
+    }
+    setTimeout(() => { this.turnIa(); }, 2000);
+
+    // setTimeout(this.turnIa, 2000);
+  }
+  useStrength() {
+    console.log(this.selectPlayer.powerstats.strength);
+    console.log(this.selectIa.powerstats.durability);
+    const sub = this.selectPlayer.powerstats.strength - this.selectIa.powerstats.durability;
+    console.log(sub);
+    if (sub < 0) {
+      this.healthIa -= Math.floor(this.selectPlayer.powerstats.strength / 2);
+    } else {
+      this.healthIa -= sub;
+    }
+    setTimeout(() => { this.turnIa(); }, 2000);
+  }
+  turnIa() {
+    const iaChoice = Math.floor(Math.random() * 10);
+    if (iaChoice < 5) {
+      console.log(this.selectIa.powerstats.power);
+      console.log(this.selectPlayer.powerstats.durability);
+      const sub = this.selectIa.powerstats.power - this.selectPlayer.powerstats.durability;
+      console.log(sub);
+      if (sub < 0) {
+        this.healthPlayer -= Math.floor(this.selectIa.powerstats.power / 2);
+      } else {
+        this.healthPlayer -= sub;
+      }
+    } else {
+      const sub = this.selectIa.powerstats.strength - this.selectPlayer.powerstats.durability;
+      console.log(sub);
+      if (sub < 0) {
+        this.healthPlayer -= Math.floor(this.selectIa.powerstats.strength / 2);
+      } else {
+        this.healthPlayer -= sub;
+      }
+    }
   }
   ngOnInit() {
     this.apiService.getHeroes().subscribe(heroes => {
